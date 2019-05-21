2b306f118221831ae98238f91ebb207086221289
Dante <dante.cnp@gmail.com>
2019-05-21 11:36:48 -0500

    ADD Header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 
-const client = ApolloClient({
+import Header from './components/Header';
+
+const client = new ApolloClient({
   uri: "http://localhost:8000/graphql",
   onError: ({networkError, graphQLErrors}) => {
     console.log('graphQLErrors', graphQLErrors);
@@ -13,7 +15,9 @@ const client = ApolloClient({
 function App() {
 	return (
     <ApolloProvider client={client}>
-    
+      <Header>
+
+      </Header>
     </ApolloProvider>
   );
 }
